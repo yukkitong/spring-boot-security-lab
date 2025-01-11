24253893f168781870afd3d184ec8ba1e09dce85
Jason Jae-sung, Bae <jason98.bae@gmail.com>
2025-01-11 14:01:49 +0900

    update controller and configuration of security

--- src/main/java/com/example/security/MainController.java
+++ src/main/java/com/example/security/MainController.java
@@ -2,10 +2,17 @@ package com.example.security;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index.html";
+    }
     
     @GetMapping("/private")
     public String pivatePage() {
--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -4,7 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -12,12 +19,40 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> {
-            auth
-                .requestMatchers("/private/**").hasRole("ADMIN")
-                .anyRequest().permitAll();
-        });
+        http
+            .csrf(config -> config.disable())
+            .httpBasic(withDefaults())
+            .formLogin(withDefaults())
+            .authorizeHttpRequests(auth -> {
+                auth
+                    .requestMatchers("/", "/index.html").permitAll()
+                    .requestMatchers("/private/**").hasRole("ADMIN")
+                    .requestMatchers("/public/**").hasRole("USER")
+                    .anyRequest().authenticated();
+            });
+            
         return http.build();
     }
+    
 
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails index = User.withUsername("index")
+            .password(encoder.encode("1234"))
+            .build();
+        UserDetails admin = User.withUsername("admin")
+            .password(encoder.encode("admin123"))
+            .roles("ADMIN")
+            .build();
+        UserDetails user = User.withUsername("user")
+            .password(encoder.encode("user123"))
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(index, admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
 }
